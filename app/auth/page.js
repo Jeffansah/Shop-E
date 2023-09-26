7c797b19d864b7ba4669f05d62ecc9c95c24fcd0
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-26 03:58:56 +0000

    Update page.js

--- app/auth/page.js
+++ app/auth/page.js
@@ -26,7 +26,7 @@ const page = () => {
           <div className="max-w-[400px] mx-auto px-2">
             <Auth
               onlyThirdPartyProviders
-              redirectTo={`${window.location.origin}/auth/callback`}
+              redirectTo={`${process.env.NEXTAUTH_URL.origin}/auth/callback`}
               supabaseClient={supabase}
               providers={["google"]}
               appearance={{ theme: ThemeSupa }}
